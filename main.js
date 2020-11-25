be87da24cf5a9e91d234f7fef1773bc1109d0af6
Ashish <ag939431@gmail.com>
2020-11-25 15:22:37 +0530

    files commited

---
+++ main.js
@@ -0,0 +1,10 @@
+// ********* Hamburger Menu  *********
+
+$(document).ready(function(){
+    $('.hamburger').on('click',function(){
+    $('.nav-list').toggleClass('active');
+    $('.hamburger').toggleClass("position");
+    });
+
+
+}) 
